1fa08f4c519e336ef59bb5f42d8e0915e4ef8c25
simona <167simonap@gmail.com>
2019-04-17 16:36:07 +0200

    bag of words, vectorizer, scaler, fit, evaluate

--- svm/svm.py
+++ svm/svm.py
@@ -1,14 +1,20 @@
 import sys
 import re
 import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
 
 
 def preprocessing(data):
     # convert reviews to lowercase
-    data = data[['Review']].applymap(lambda x: x.lower()) 
+    data[['Review']] = data[['Review']].applymap(lambda x: x.lower()) 
 
     # strip punctuation
-    data = data[['Review']].applymap(lambda x: re.sub(r'[^\w\s]','',x))
+    data[['Review']] = data[['Review']].applymap(lambda x: re.sub(r'[^\w\s]','',x))
+    # convert new line to space
+    data[['Review']] = data[['Review']].applymap(lambda x: re.sub(r'\n',' ',x))
 
     # remove stopwords
     stopwords = set(['a','ako','ali','bi','bih','bila','bili','bilo','bio','bismo','biste','biti',
@@ -25,20 +31,32 @@ def preprocessing(data):
     'su','sve','svi','svog','svoj','svoja','svoje','svom','ta','tada','taj','tako','te',
     'tebe','tebi','ti','to','toj','tome','tu','tvoj','tvoja','tvoje','u','uz','vam','vama',
     'vas','vas','vasa','vase','vec','vi','vrlo','za','zar','ce','cemo','cete','ces','cu','sta','sto']) 
-    # NAPOMENA!!! set ne cuva redosled reci iz liste
-    # ukoliko je redosled bitan, promeni set u neku strukturu koja cuva redosled(orderedDict?)
-    data = data[['Review']].applymap(lambda x: ' '.join((stopwords ^ set(x.split(' '))) & set(x.split(' '))))
 
+    data[['Review']] = data[['Review']].applymap(lambda x: ' '.join((stopwords ^ set(x.split(' '))) & set(x.split(' '))))
+    
     return data
 
-def vectorization():
-    pass
+def bag_of_words(data):
+    all_dicts = []
+    for row in data.iterrows():
+        content = row[1]['Review']
+        content = content.split(' ')
+        try:
+            content.remove('')
+        except:
+            pass
+        word_dict = {el:1 for el in set(content)}
+        all_dicts.append(word_dict)   
+    return all_dicts
 
-def fit():
-    pass
+def fit(X, y):
+    clf = LinearSVC(C=1)
+    clf.fit(X, y)
+    return clf
 
-def evaluate():
-    pass
+def evaluate(clf, X):
+    result = clf.predict(X)
+    return result
 
 def load_data(file_path):
     return pd.read_csv(file_path, sep='\t')
@@ -56,13 +74,30 @@ def main():
     test_set = load_data(test_set_path)
     train_set, validation_set = create_validation_set(train_set)
 
-    preprocessing(train_set)
-    preprocessing(validation_set)
+    train_set = preprocessing(train_set)
+    validation_set = preprocessing(validation_set)
+    test_set = preprocessing(test_set)
 
+    all_dicts = bag_of_words(train_set)
+    val_dicts = bag_of_words(validation_set)
+    test_dicts = bag_of_words(test_set)
 
-    vectorization()
-    fit()
-    evaluate()
+    dict_vectorizer = DictVectorizer(sparse=False)
+    train_X = dict_vectorizer.fit_transform(all_dicts)
+    validation_X = dict_vectorizer.transform(val_dicts)
+    test_X = dict_vectorizer.transform(test_dicts)
+
+    scaler = StandardScaler()
+    train_X = scaler.fit_transform(train_X, train_set['Sentiment'])
+    validation_X = scaler.transform(validation_X)
+    test_X = scaler.transform(test_X)
+
+    clf = fit(train_X, train_set['Sentiment'])
+    y_pred = evaluate(clf, validation_X)
+    y_pred_test = evaluate(clf, test_X)
+
+    print('validation ', accuracy_score(validation_set['Sentiment'], y_pred))
+    print('test ', accuracy_score(test_set['Sentiment'], y_pred_test))
 
 if __name__ == "__main__":
     main()
